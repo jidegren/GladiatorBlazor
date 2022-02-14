64f681fb93fc4c8d5e1399f2e136e5b18968d311
Robin Jidegren <robin_jide@hotmail.com>
2022-02-14 10:37:46 +0100

    First commit

---
+++ GladiatorBlazor/Models/GameManager.cs
@@ -0,0 +1,30 @@
+﻿namespace GladiatorBlazor.Models
+{
+    public class GameManager
+    {
+        public bool IsRunning { get; set; } = false;
+        public async void MainLoop()
+        {
+            IsRunning = true;
+
+            while (IsRunning)
+            {
+
+            }
+        }
+
+        public void StartGame()
+        {
+            if (IsRunning)
+            {
+                MainLoop();
+            }
+        }
+
+        public void GameOver()
+        {
+            IsRunning = false;
+        }
+
+    }
+}
---
+++ GladiatorBlazor/Models/GladiatorModel.cs
@@ -0,0 +1,24 @@
+﻿namespace GladiatorBlazor.Models
+{
+    public class GladiatorModel
+    {
+        public double Health { get; set; } 
+        public double Strength { get; set; }
+        public double Endurance { get; set; }
+        public void Attack()
+        {
+
+        }
+        public void Avoid()
+        {
+
+        }
+
+        public void Surrender()
+        {
+
+        }
+
+
+    }
+}
